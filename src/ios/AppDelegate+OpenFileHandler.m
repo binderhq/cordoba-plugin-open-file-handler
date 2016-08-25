3fcc21a421e85d6ef75d62bf8e9694e58363628e
Peter Price <peter@binder.works>
2016-08-25 14:59:02 +1000

    IT WORKS!

--- src/ios/AppDelegate+OpenFileHandler.m
+++ src/ios/AppDelegate+OpenFileHandler.m
@@ -11,8 +11,17 @@
 
 - (BOOL) application:(UIApplication *)application openURL:(NSURL *) url sourceApplication:(NSString *) sourceApplication annotation:(id) annotation
 {
-    NSLog(@"The file contained: %@",url.absoluteString);
-
+    // Set modified date of file
+    NSError* error;
+    NSDictionary* attr = [NSDictionary dictionaryWithObjectsAndKeys:[NSDate date], NSFileModificationDate, NULL];
+    
+    [[NSFileManager defaultManager] setAttributes: attr ofItemAtPath:url.path error: NULL];
+    
+    if(error != nil) {
+        NSLog(@"Error setting modification date: %@", error.localizedDescription);
+    } else {
+        NSLog(@"Set modification date on file");
+    }
     return YES;
 }
 
