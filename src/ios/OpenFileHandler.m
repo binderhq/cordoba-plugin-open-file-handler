a9bb4ce8bc1e89ebf2e783146a6e3630d7a4f118
Peter Price <peter@binder.works>
2016-08-26 14:33:25 +1000

    Set filename to nil once it has been retrieved.

--- src/ios/OpenFileHandler.m
+++ src/ios/OpenFileHandler.m
@@ -12,5 +12,7 @@
 - (void)checkForOpenedFile:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:self.filename];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+    filename = nil;
 }
 @end
